dbbd14a7c09d680f5940474edd793f9171d1c125
Mudassar-Shoukat <mudassarshoukat44@gmail.com>
2024-03-06 12:51:55 +0500

    change of click event in vue js

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,29 +1,29 @@
 <template>
-
     <div>
-        <h3>Increment Button</h3>
-        <button @click="increment()">{{ `Increment is ${num}` }}</button>
+      <h3>Increment Button</h3>
+      <button @click="increment()">{{ `Increment is: ${num}` }}</button>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     data() {
-        return {
-            num: 0,
-        }
+      return {
+        num: 0,
+      };
     },
     methods: {
-        increment() {
-            this.num++;
-            if (this.num >= 10) {
-                alert("This number is greater than 10 number ");
-                this.num = 0;
-            }
-            console.log(this.num);
+      increment() {
+        this.num += 1;
+        if (this.num >= 10) {
+          alert("This number is greater than 10.");
+          this.num = 0;
         }
+        console.log(this.num);
+      },
     },
-}
-</script>
+  };
+  </script>
+  
 
 <style scoped></style>
