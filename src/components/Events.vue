379befdcb8e9a5c505decc24f86eefb6aef2b59d
Mudassar-Shoukat <mudassarshoukat44@gmail.com>
2024-03-06 12:35:39 +0500

    Practice of event in vue js 2

---
+++ src/components/Events.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div>
+        <h3>Increment Button</h3>
+        <button @click="increment()">{{ `Increment is ${num}` }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num: 0,
+        }
+    },
+    methods: {
+        increment() {
+            this.num++;
+            if (this.num >= 10) {
+                alert("This number is greater than 10 number ");
+                this.num = 0;
+            }
+            console.log(this.num);
+        }
+    },
+}
+</script>
+
+<style scoped></style>
