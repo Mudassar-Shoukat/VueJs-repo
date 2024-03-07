71f524a2a0a298a0834766137099c26f1829ffc9
Mudassar-Shoukat <mudassarshoukat44@gmail.com>
2024-03-07 10:02:47 +0500

    Add code in keyboard enent

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-      <h3>Increment Button</h3>
+      <div id="click_event">
+      <h3> Click Event</h3>
       <button @click="increment()">{{ `Increment is: ${num}` }}</button>
+    </div>
+    <div id="keyboard_event">
+      <h3>Keyborad Event</h3>
+      <span> Enter name of User:  <input type="text" v-on:keydown="down"></span>
+      <h2 >Press key is:- {{ KeyboardEvent }}</h2>
+    </div>
     </div>
   </template>
   
@@ -10,9 +17,11 @@
     data() {
       return {
         num: 0,
+        KeyboardEvent:"",
       };
     },
     methods: {
+      // This code is used for click event in vue js 2
       increment() {
         this.num += 1;
         if (this.num >= 10) {
@@ -21,9 +30,30 @@
         }
         console.log(this.num);
       },
+        // This code is used for keyboard event in vue js 2
+        down(enterkey){
+          this.KeyboardEvent=enterkey.key,
+          console.log(enterkey.key);
+        }
     },
   };
   </script>
   
 
-<style scoped></style>
+<style>
+#click_event{
+  margin-left: 5px;
+  background: rgb(222, 212, 212);
+  height: 100px;
+
+  
+  
+}
+#keyboard_event{
+  margin-left: 5px;
+  background: rgb(234, 229, 229);
+  height: 100px;
+  
+}
+
+</style>
