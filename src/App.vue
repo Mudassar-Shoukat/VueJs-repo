82e00e6de898f5f6a8f67ebb84812a8f499e46cd
Mudassar-Shoukat <mudassarshoukat44@gmail.com>
2024-03-20 09:52:22 +0500

    Popup Box

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,17 @@
 <template>
-  <div id="calculator">
-    <Calculatorscreen :current-value="currentValue" @input-value="handleInputValue" />
-    <Calculatorbuttons :current-value="currentValue" @input-value="handleInputValue" 
-     @calculate="calculateResult"/>
+  <div>
+<buttontransitions/>
   </div>
 </template>
 
 <script>
-import Calculatorscreen from "./components/Calculatorscreen.vue";
-import Calculatorbuttons from "./components/Calculatorbuttons.vue";
-
-export default {
-  components: {
-    Calculatorscreen,
-    Calculatorbuttons,
-  },
-  data() {
-    return {
-      currentValue: "",
-      previousValue: "",
-      operator: "",
-    };
-  },
-  methods: {
-    handleInputValue(value) {
-      console.log(value);
-      this.currentValue += value;
-     
-    },
-    calculateResult() {
-      if (this.operator && this.previousValue !== null) {
-        let result;
-        switch (this.operator) {
-          case "+":
-            result =
-              parseFloat(this.previousValue) + parseFloat(this.currentValue);
-             
-            break;
-          case "-":
-            result =
-              parseFloat(this.previousValue) - parseFloat(this.currentValue);
-              
-            break;
-          case "*":
-            result =
-              parseFloat(this.previousValue) * parseFloat(this.currentValue);
-            break;
-          case "/":
-            if (this.currentValue === "0") {
-              result = "Error: Division by zero";
-            } else {
-              result =
-                parseFloat(this.previousValue) / parseFloat(this.currentValue);
-            }
-            break;
-          default:
-            break;
-        }
-        this.currentValue = result.toString();
-        this.previousValue = null;
-        this.operator = null;
-      }
-    },
+import Buttontransitions from './Buttontransitions.vue'
+  export default {
+  components: { Buttontransitions },
     
-  },
-};
-</script> 
-
-
+  }
+</script>
 
 <style scoped>
-#calculator {
-  background-color: #cdd2a2;
-  border-radius: 5px;
-  padding: 1em 1em 1em 1em;
-  margin: 10px auto;
-  width: 300px;
-  display: table;
-}
+
 </style>
